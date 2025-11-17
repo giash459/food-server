9a477fc964ca2443f3041f82ce8154c20b7cb068
giash459 <mdgiash999ctg@gmail.com>
2025-11-17 22:47:20 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://foodUser:izJfQR69dunyGnXJ@cluster0.ukeaca2.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Food server is running')
+})
+
+async function run() {
+    try {
+        await client.connect();
+
+        const db = client.db('food_db');
+        const productsCollection = db.collection('products');
+
+        app.get('/products', async (req, res) => {
+            const cursor = productsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId.id }
+            const result = await productsCollection.findOne((query));
+            res.send(result)
+        })
+
+        app.post('/products', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productsCollection.insertOne(newProduct);
+        })
+
+        app.patch('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateProduct = req.body;
+            const query = { _id: new ObjectId(id) }
+            const update = {
+                $set: {
+                    name: updateProduct.name,
+                    price: updateProduct.price
+                }
+            }
+            const result = await productsCollection.updateOne(query, update)
+            res.send(result)
+        })
+
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await productsCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally { }
+}
+
+run().catch(console.dir)
+
+app.listen(port, () => {
+    console.log(`Food server is running on port: ${port}`);
+})
